ee0fe565b5cc1a3e376556a25cd9d4858a75a3c4
Muskan Sukhija <sukhijamuskan23@gmail.com>
2026-06-02 22:33:00 +0530

    Implement Roman numeral to integer conversion

---
+++ Day03_RomanToInt.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        for(int i = 0; i<s.length() ; i++){
+            int curr = value(s[i]);
+            int next = ((i+1)<s.length()) ? value(s[i+1]) : 0;
+            if (curr < next ){
+                ans -= curr;
+            }else{
+                ans += curr;
+            }
+        }
+        return ans;
+    }
+    int value(char c){
+        if (c =='I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+        return 0;
+
+    }
+};
